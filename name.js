c26ea6fa5922ab3f42acc550c483ecb63cd00a93
programmer262 <65510490+programmer262@users.noreply.github.com>
2020-06-25 01:41:39 +0100

    Add files via upload

---
+++ name.js
@@ -0,0 +1,9 @@
+newFunction();
+
+function newFunction() {
+    var A = prompt('please enter your name')
+    alert(`welcome to our corporation MR. (MRS.) ${A}`)
+}
+
+
+// its an alert that helps to say hi to the user
